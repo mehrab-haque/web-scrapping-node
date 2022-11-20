5e35ef85e94ef5fa9190d31027dcaa4d35bd4422
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-11-21 00:01:06 +0600

    cheerio+axios+express done

---
+++ app.js
@@ -0,0 +1,48 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const parseExample1=new Promise(async (resolve,_)=>{
+    try{
+        var res= await axios.get('https://scrapeme.live/shop/')
+        const $ = cheerio.load(res.data);
+        const pokemonNames = $('.woocommerce-loop-product__title')
+            .map((_, product) => {
+                const $product = $(product);
+                return $product.text()
+            })
+            .toArray();
+        resolve(pokemonNames)
+    }catch(e){
+        resolve('error occurred')
+    }
+})
+
+const parseExample2=new Promise(async (resolve,_)=>{
+    try{
+        var res= await axios.get('https://en.wikipedia.org/wiki/Districts_of_Bangladesh')
+        const $ = cheerio.load(res.data);
+        var districts=[]
+        $('.wikitable tbody tr').map((_,row)=>{
+            var currDistrict=$($(row).children()[0]).find('a').text()
+            if(currDistrict.length>0)districts.push(currDistrict)
+
+        })
+        resolve(districts)
+    }catch(e){
+        resolve('error occurred')
+    }
+})
+
+app.get('/scrap', async (req, res) => {
+    var allResults=await Promise.all([parseExample1,parseExample2])
+    res.send(allResults)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+

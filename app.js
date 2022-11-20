6437177021f32e8de61979f2bf6c03d06eb57c23
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-11-21 00:07:31 +0600

    change

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
-
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 const parseExample1=new Promise(async (resolve,_)=>{
-    try{
-        var res= await axios.get('https://scrapeme.live/shop/')
-        const $ = cheerio.load(res.data);
-        const pokemonNames = $('.woocommerce-loop-product__title')
-            .map((_, product) => {
-                const $product = $(product);
-                return $product.text()
-            })
-            .toArray();
-        resolve(pokemonNames)
-    }catch(e){
-        resolve('error occurred')
-    }
-})
-
-const parseExample2=new Promise(async (resolve,_)=>{
     try{
         var res= await axios.get('https://en.wikipedia.org/wiki/Districts_of_Bangladesh')
         const $ = cheerio.load(res.data);
@@ -32,6 +16,21 @@ const parseExample2=new Promise(async (resolve,_)=>{
 
         })
         resolve(districts)
+    }catch(e){
+        resolve('error occurred')
+    }
+})
+
+const parseExample2=new Promise(async (resolve,_)=>{
+    try{
+        var res= await axios.get('https://scrapeme.live/shop/')
+        const $ = cheerio.load(res.data);
+        const pokemonNames = $('.woocommerce-loop-product__title')
+            .map((_, product) => {
+                return $(product).text()
+            })
+            .toArray();
+        resolve(pokemonNames)
     }catch(e){
         resolve('error occurred')
     }
